10230fad16c6e018336f86ae19aabf9043a7385c
khumbaka <khumbaka.nudriarto@asyx.com>
2022-12-28 18:35:51 +0700

    Initialize Migrations

---
+++ GoogleApi/Migrations/20221228113434_Initialize.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoogleApi.Migrations
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GoogleEmails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Subject = table.Column<string>(maxLength: 512, nullable: false),
+                    Body = table.Column<string>(nullable: true),
+                    From = table.Column<string>(maxLength: 256, nullable: false),
+                    Date = table.Column<string>(maxLength: 512, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GoogleEmails", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GoogleEmails");
+        }
+    }
+}
